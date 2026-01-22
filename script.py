df3bda8f7a9e5efa6d0ca183090e9687546a87c9
Romain <rnayrac@hotmail.com>
2026-01-22 18:24:09 +0100

    v0.2

--- script.py
+++ script.py
@@ -14,9 +14,10 @@ game = [
 ]
 
 piece = {}
-tour="w"
+tour="w" # defini le début du tour au blanc
+nottour="b"
 
-for line in game:
+for line in game:  #definir les position des piece
     for p in line:
         if "p" in p:
             piece[p]={
@@ -50,26 +51,91 @@ for line in game:
 
 
 def postovalue(pos):
-    return game[int(pos/10)-1][pos-10*(int(pos/10))-1]
+    return game[pos[0]-1][pos[1]-1]
 
-def mouvtour():
-    if 
+def wichside(pini,pfin):                     #8 cas differend   hg h hd
+    if pini[0]==pfin[0] and pini[1]<pfin[1]: # meme ligne        g P  d
+        return "d"                           #                  bg b bd
+    if pini[1]==pfin[1] and pini[0]<pfin[0]:
+        return "h"
+    if pini[0]==pfin[0] and pini[1]>pfin[1]:
+        return "g"
+    if pini[1]==pfin[1] and pini[0]>pfin[0]:
+        return "b"
+    if pini[0]<pfin[0] and pini[1]<pfin[1]:
+        return "hd"
+    if pini[0]<pfin[0] and pini[1]>pfin[1]:
+        return "hg"
+    if pini[0]>pfin[0] and pini[1]>pfin[1]:
+        return "bg"
+    if pini[0]>pfin[0] and pini[1]<pfin[1]:
+        return "bd"
 
-def deplacer(pini,pfin): # return false si le mouvement est impossible pini et pfin systeme de 1*1 a 8*8
+def verif(pos):
+    if game[pos[0]-1][pos[1]+1]=="": return False
+    elif nottour in game[pos[0]-1][pos[1]+1]: return False
+    else: return True
+
+def mouvtour(pini,pfin,n):
+    side = wichside(pini,pfin)
+    print(side)
+    if side == "d" or side == "g":
+        for i in range:
+            #verif([,])
+            pass
+
+
+    game[pini[0]-1][pini[1]-1]=""
+    game[pfin[0]-1][pfin[1]-1]=tour+"t"+str(n)
+    pass
+
+def deplacer(pini,pfin): # return false si le mouvement est impossible pini et pfin systeme de liste 1*1 a 8*8
+    if pini==pfin:
+        return False
     piece=postovalue(pini)
-    if "1" in piece:
+    if "1" in piece: # detection du numéro de piece
         n=1
-    if "2" in piece:
+    elif "2" in piece:
         n=2
+    elif "3" in piece:
+        n=3
+    elif "4" in piece:
+        n=4
+    elif "5" in piece:
+        n=5
+    elif "6" in piece:
+        n=6
+    elif "7" in piece:
+        n=7
+    elif "8" in piece:
+        n=8
+    else:
+        n=False # False si pas de numero roi/reine
+    
     if tour not in piece:  # verification si c'est le bon joueur
         return False
+    
     if "t" in piece:
         mouvtour(pini,pfin,n)
+    if "c" in piece:
+        mouvcavalier(pini,pfin,n)
+    if "f" in piece:
+        mouvfou(pini,pfin,n)
+    if "p" in piece:
+        mouvpion(pini,pfin,n)
+    if "r" in piece:
+        mouvroi(pini,pfin)
+    if "q" in piece:
+        mouvqueen(pini,pfin)
         pass
 
 
 
 
 if __name__ == "__main__":
+    for line in game : print(line)
+    mouvtour([8,1],[5,5],1)
+    for line in game : print(line)
+    #print(postovalue([7,7]))
     #for line in game : print(line)
     #print(piece)
